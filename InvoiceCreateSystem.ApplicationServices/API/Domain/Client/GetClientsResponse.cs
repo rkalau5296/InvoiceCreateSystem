3de45b846ae078f19e288694fc26c0f90568ccf3
Rafał Kalata <rafal.kalata.itservice@gmail.com>
2024-11-19 18:36:03 +0100

    modify response

--- InvoiceCreateSystem.ApplicationServices/API/Domain/Client/GetClientsResponse.cs
+++ InvoiceCreateSystem.ApplicationServices/API/Domain/Client/GetClientsResponse.cs
@@ -1,7 +1,7 @@
 ﻿namespace InvoiceCreateSystem.ApplicationServices.API.Domain.Client
 {
     using InvoiceCreateSystem.ApplicationServices.API.Domain.Models;
-    public class GetClientsResponse :  ResponseBase<Client>
+    public class GetClientsResponse :  ResponseBase<List<Client>>
     {
     }
 }
